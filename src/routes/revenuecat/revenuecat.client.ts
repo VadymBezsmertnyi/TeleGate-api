1cee8d5addfc228e0c96f15c62bca3c827817310
Vadym <vados199368@gmail.com>
2025-08-21 08:52:33 +0300

    Refactor RevenueCat client to support versioning and add read-only client

--- src/routes/revenuecat/revenuecat.client.ts
+++ src/routes/revenuecat/revenuecat.client.ts
@@ -1,14 +1,32 @@
-import axios from "axios";
+import axios, { AxiosInstance } from "axios";
 
-const REVENUECAT_API_KEY = process.env.REVENUECAT_API_KEY;
-if (!REVENUECAT_API_KEY) console.warn("REVENUECAT_API_KEY не налаштований");
+const REVENUECAT_API_KEY_V1 = process.env.REVENUECAT_API_KEY_V1;
+const REVENUECAT_API_KEY_V1_READ_ONLY =
+  process.env.REVENUECAT_API_KEY_V1_READ_ONLY;
+if (!REVENUECAT_API_KEY_V1)
+  console.warn("REVENUECAT_API_KEY_V1 не налаштований");
+if (!REVENUECAT_API_KEY_V1_READ_ONLY)
+  console.warn("REVENUECAT_API_KEY_V1_READ_ONLY не налаштований");
 
-const revenuecatClient = axios.create({
-  baseURL: "https://api.revenuecat.com/v1",
-  headers: {
-    Authorization: `Bearer ${REVENUECAT_API_KEY}`,
-    "Content-Type": "application/json",
-  },
-});
+const createRevenueCatClient = (
+  apiKey: string,
+  isV2: boolean = false
+): AxiosInstance => {
+  return axios.create({
+    baseURL: `https://api.revenuecat.com/v${isV2 ? "2" : "1"}`,
+    headers: {
+      "X-Platform": "ios",
+      Authorization: `Bearer ${apiKey}`,
+      "Content-Type": "application/json",
+    },
+    timeout: 10000,
+  });
+};
 
-export default revenuecatClient;
+const revenuecatClientV1 = createRevenueCatClient(REVENUECAT_API_KEY_V1 || "");
+const revenuecatReadOnlyClientV2 = createRevenueCatClient(
+  REVENUECAT_API_KEY_V1_READ_ONLY || "",
+  true
+);
+
+export { revenuecatClientV1, revenuecatReadOnlyClientV2 };
