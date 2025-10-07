52720c3abd39d6de37a3a79b60177b725eee7aa3
Vadym <vados199368@gmail.com>
2025-10-07 09:01:55 +0300

    Add detailed type definitions for CoinPaprika data structures

--- src/coin-diviner-ai/hooks/coinpaprika/coinpaprika.types.ts
+++ src/coin-diviner-ai/hooks/coinpaprika/coinpaprika.types.ts
@@ -7,3 +7,55 @@ export type TCoinMainInfo = {
   is_active: boolean;
   type: "coin" | "token";
 };
+
+export type TCoinTag = {
+  id: string;
+  name: string;
+  coin_counter: number;
+  ico_counter: number;
+};
+
+export type TCoinTeam = {
+  id: string;
+  name: string;
+  position: string;
+};
+
+export type TCoinLink = {
+  explorer: string[];
+  facebook: string[];
+  reddit: string[];
+  source_code: string[];
+  website: string[];
+  youtube: string[];
+};
+
+export type TCoinLinkExtended = {
+  url: string;
+  type: string;
+  stats?: { [key: string]: number };
+};
+
+export type TCoinWhitepaper = {
+  link: string;
+  thumbnail: string;
+};
+
+export type TCoinFullInfo = TCoinMainInfo & {
+  description: string;
+  logo: string;
+  tags: TCoinTag[];
+  team: TCoinTeam[];
+  links: TCoinLink;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  links_extended: TCoinLinkExtended[];
+  whitepaper: TCoinWhitepaper;
+  first_data_at: string;
+  last_data_at: string;
+};
