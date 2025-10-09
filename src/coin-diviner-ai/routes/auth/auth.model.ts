d57d5eda9efeed7954f805d1a13ea2e648b91edb
Vadym <vados199368@gmail.com>
2025-10-09 20:29:08 +0300

    Enhance authentication model and schemas with additional fields for user management

--- src/coin-diviner-ai/routes/auth/auth.model.ts
+++ src/coin-diviner-ai/routes/auth/auth.model.ts
@@ -5,6 +5,9 @@ const authSM = new Schema(
     email: { type: String, required: true },
     password: { type: String, required: true },
     phone: { type: String, required: false },
+    isBlocked: { type: Boolean, default: false },
+    isVerified: { type: Boolean, default: false },
+    role: { type: String, enum: ["user", "admin"], default: "user" },
   },
   {
     timestamps: true,
--- src/coin-diviner-ai/routes/auth/auth.schemas.ts
+++ src/coin-diviner-ai/routes/auth/auth.schemas.ts
@@ -4,29 +4,32 @@ export const authSchema = z.object({
   _id: z.any(),
   email: z.string(),
   phone: z.string().nullable().optional(),
+  isBlocked: z.boolean().default(false),
+  isVerified: z.boolean().default(false),
+  role: z.enum(["user", "admin"]).default("user"),
   createdAt: z.date(),
   updatedAt: z.date(),
 });
 
 export const loginSchema = z.object({
-  email: z.string().email(),
+  email: z.email(),
   password: z.string().min(6),
 });
 
 export const registerSchema = z.object({
-  email: z.string().email(),
+  email: z.email(),
   password: z.string().min(6),
   phone: z.string().optional(),
 });
 
 export const createAuthSchema = z.object({
-  email: z.string().email(),
+  email: z.email(),
   password: z.string().min(6),
   phone: z.string().optional(),
 });
 
 export const updateAuthSchema = z.object({
-  email: z.string().email().optional(),
+  email: z.email().optional(),
   password: z.string().min(6).optional(),
   phone: z.string().optional(),
 });
