8a20c462c14efed536f1fe40149bf320ba21be94
Vadym <vados199368@gmail.com>
2025-10-20 08:59:32 +0300

    Refactor coin data extraction in getDataFavoriteData function for improved clarity and structure

--- src/coin-diviner-ai/routes/favorites/favorites.helps.ts
+++ src/coin-diviner-ai/routes/favorites/favorites.helps.ts
@@ -4,7 +4,24 @@ export const getDataFavoriteData = (favorite: any) => {
   if (!favorite || typeof favorite !== "object" || !favorite._id) return null;
 
   const coin =
-    favorite.coinId && typeof favorite.coinId === "object" ? favorite : null;
+    favorite.coinId && typeof favorite.coinId === "object"
+      ? {
+          ...favorite.coinId,
+          lastUpdatedCoinPaprika: favorite.coinId.lastUpdatedCoinPaprika
+            ? new Date(favorite.coinId.lastUpdatedCoinPaprika)
+            : undefined,
+          lastUpdatedCoinGecko: favorite.coinId.lastUpdatedCoinGecko
+            ? new Date(favorite.coinId.lastUpdatedCoinGecko)
+            : undefined,
+          createdAt: favorite.coinId.createdAt
+            ? new Date(favorite.coinId.createdAt)
+            : undefined,
+          updatedAt: favorite.coinId.updatedAt
+            ? new Date(favorite.coinId.updatedAt)
+            : undefined,
+          source: favorite.coinId.source,
+        }
+      : null;
 
   const responseData: TFavoriteRecord = {
     _id: favorite._id.toString(),
